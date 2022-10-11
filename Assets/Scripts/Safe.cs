00077aeb549e9ef5a160349bfa0c13a625c8a65d
DoronZ <doron88@gmail.com>
2022-10-11 15:30:06 +0300

    initial commit

---
+++ Assets/Scripts/Safe.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Safe : MonoBehaviour
+{
+    public string _correctCode;
+    public string _enteredCode;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void appendString(string str)
+    {
+        _enteredCode += str;
+        Debug.Log("enteredCode: " + _enteredCode);
+
+        if (_correctCode == _enteredCode)
+        {
+            Debug.Log("correct!!!");
+        }        
+    }
+}
