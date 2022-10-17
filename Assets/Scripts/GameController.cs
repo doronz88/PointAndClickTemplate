e4f55244de9084bd0e64e6c2a17a5c2fec13556c
DoronZ <doron88@gmail.com>
2022-10-18 01:27:04 +0300

    add sample background music

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
+    private AudioSource _audioSource;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+
+        if (!_audioSource)
+        {
+            _audioSource = GetComponent<AudioSource>();
+            PlayMusic();
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +28,20 @@ public class GameController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void PlayMusic()
+    {
+        if (_audioSource.isPlaying)
+        {
+            return;
+        }
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
     }
 
     public void LoadScene(string sceneName)
